fb7bb6a2b3b321d8f096a899fda0570cf35acd9e
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-06-12 17:51:05 -0400

    fixed formatting that broke test

--- utils/generateSvg.test.js
+++ utils/generateSvg.test.js
@@ -14,7 +14,7 @@ describe('generateSvg', () => {
             );
             expect(createdSvg).toBe(`<svg version="1.1" width="300" height="200" xmlns="htttp://www.w3.org/2000/svg">
 
-            <circle cx="150" cy="100" r="80" fill="red" stroke="black" stroke-width="3"/>
+            <circle cx="150" cy="100" r="80" fill="red" stroke="black"/>
 
             <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">???</text>
             </svg>`);
